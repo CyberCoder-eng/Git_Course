7bb8197ed43eda2f59b4af66899a095fe879d071
monusarkar <monusarkar1013@gmail.com>
2024-12-17 00:56:38 +0530

    mygencode

--- Binary Search GFG/kth_ele_two_array.py
+++ Binary Search GFG/kth_ele_two_array.py
@@ -1,6 +1,7 @@
-# Brute Froce 
+# Brute Froce
 
-def func(a, b, k):
+
+def func1(a, b, k):
 
     res = a + b
     print(res)
@@ -8,10 +9,46 @@ def func(a, b, k):
     print(ans)
     return ans[k - 1]
 
+
+a = [2, 3, 6, 7, 9]
+b = [1, 4, 8, 10]
+k = 5
+print(func1(a, b, k))
+
+
+# Optimize solution
+
+
+def func(a, b, k):
+    n, m = len(a), len(b)
+    i = j = count = 0
+
+    while i < n and j < m:
+        if a[i] < b[j]:
+            count += 1
+            if count == k:
+                return a[i]
+            i += 1
+        else:
+            count += 1
+            if count == k:
+                return b[j]
+            j += 1
+    while i < n:
+        count += 1
+        if count == k:
+            return a[i]
+        i += 1
+
+    while j < m:
+        count += 1
+        if count == k:
+            return b[j]
+        j += 1
+    return -1
+
+
 a = [2, 3, 6, 7, 9]
 b = [1, 4, 8, 10]
 k = 5
 print(func(a, b, k))
-
-
-# Optimize solution 
