02e67eb61eef15cd0f023b05fbf7449422833f33
monusarkar <monusarkar1013@gmail.com>
2024-12-30 21:13:08 +0530

    two file upload

---
+++ Hashing/intersection_two_array.py
@@ -0,0 +1,14 @@
+def func(a, b):
+    res = []
+    set_b = set(b)
+
+    for num in a:
+        if num in set_b:
+            res.append(num)
+            set_b.remove(num)
+    return res
+
+
+a = [1, 2, 1, 3, 1]
+b = [3, 1, 3, 4, 1]
+print(func(a, b))
---
+++ Hashing/union_array_duplicate.py
@@ -0,0 +1,19 @@
+def union_arr(a, b):
+    return len(set(a).union(set(b)))
+
+
+a = [1, 2, 3, 4, 5]
+b = [1, 2, 3]
+print(union_arr(a, b))
+
+
+def func(a, b):
+    hash_map = set(b)
+    for num in a:
+        hash_map.add(num)
+    return len(hash_map)
+
+
+a = [1, 2, 1, 1, 2]
+b = [2, 2, 1, 2, 1]
+print(func(a, b))
