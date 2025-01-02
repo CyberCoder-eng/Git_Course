bc2533ab5ed4aa416764f4ca0e5a117af0088c43
monusarkar <monusarkar1013@gmail.com>
2025-01-02 23:52:55 +0530

    sum with k

---
+++ Hashing/subarray_with_sum_k.py
@@ -0,0 +1,19 @@
+# Optimal Code
+
+
+def func(arr, k):
+    count = 0
+    total = 0
+    hash_map = {0: 1}
+    n = len(arr)
+    for i in range(n):
+        total += arr[i]
+        if total - k in hash_map:
+            count += hash_map[total - k]
+        hash_map[total] = hash_map.get(total, 0) + 1
+    return count
+
+
+arr = [10, 2, -2, -20, 10]
+k = -10
+print(func(arr, k))
