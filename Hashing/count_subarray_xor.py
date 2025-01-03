255a87e6c657a121f5435ff45166626b67ab0688
monusarkar <monusarkar1013@gmail.com>
2025-01-03 21:15:45 +0530

    subarray xor

---
+++ Hashing/count_subarray_xor.py
@@ -0,0 +1,21 @@
+# Optimal Code
+
+
+def func(arr, k):
+    hash_map = dict()
+    count = 0
+    xor = 0
+    for i in range(len(arr)):
+        xor ^= arr[i]
+        if xor == k:
+            count += 1
+
+        if xor ^ k in hash_map:
+            count += hash_map[xor ^ k]
+        hash_map[xor] = hash_map.get(xor, 0) + 1
+    return count
+
+
+arr = [4, 2, 2, 6, 4]
+k = 6
+print(func(arr, k))
