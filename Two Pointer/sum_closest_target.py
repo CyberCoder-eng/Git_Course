6ed73dc5592750233255544147b3aef63d23ce63
monusarkar <monusarkar1013@gmail.com>
2025-01-06 23:39:12 +0530

    closesttarget

---
+++ Two Pointer/sum_closest_target.py
@@ -0,0 +1,28 @@
+# Optimize Code
+
+
+def func(arr, target):
+    arr.sort()
+    n = len(arr)
+    left = 0
+    right = n - 1
+    res = []
+    total = float("inf")
+
+    while left < right:
+        curr_total = arr[left] + arr[right]
+        if abs(target - curr_total) < abs(target - total):
+            total = curr_total
+            res = [arr[left], arr[right]]
+        if curr_total < target:
+            left += 1
+        elif curr_total > target:
+            right -= 1
+        else:
+            break
+    return res
+
+
+arr = [5, 2, 7, 1, 4]
+target = 10
+print(func(arr, target))
