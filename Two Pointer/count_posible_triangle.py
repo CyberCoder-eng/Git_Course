fe6341493aa7475c61eac1fa96ee9047103ef16b
monusarkar <monusarkar1013@gmail.com>
2025-01-08 20:26:34 +0530

    count triangles

---
+++ Two Pointer/count_posible_triangle.py
@@ -0,0 +1,20 @@
+def countTriangles(arr):
+    count = 0
+    arr.sort()
+    n = len(arr)
+    for i in range(n - 1, 1, -1):
+        left = 0
+        right = i - 1
+
+        while left < right:
+            if arr[left] + arr[right] > arr[i]:
+                count += right - left
+                right -= 1
+            else:
+                left += 1
+
+    return count
+
+
+arr = [4, 6, 3, 7]
+print(countTriangles(arr))
