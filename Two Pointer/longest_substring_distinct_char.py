80b6ea4439fe6c3099cf9d1f1979c87164e1d644
monusarkar <monusarkar1013@gmail.com>
2025-01-11 20:45:51 +0530

    longest unique char

---
+++ Two Pointer/longest_substring_distinct_char.py
@@ -0,0 +1,31 @@
+def func(s):
+    res = ""
+    count = 0
+    for char in s:
+        if char not in res:
+            res += char
+            count = max(len(res), count)
+        else:
+            res = res[res.index(char) + 1 :] + char
+    return count
+
+
+s = "geeksforgeeks"
+print(func(s))
+
+
+def longestUniquechar(s):
+    hash_map = dict()
+    count = 0
+    res = 0
+    for char in range(len(s)):
+        hash_map[s[char]] = hash_map.get(s[char], 0) + 1
+        while hash_map[s[char]] > 1:
+            hash_map[s[count]] = hash_map.get(s[count], 0) - 1
+            count += 1
+        res = max(res, char - count + 1)
+    return res
+
+
+s = "geeksforgeeks"
+print(longestUniquechar(s))
