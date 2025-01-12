624dc12971500e6bb29c18b20e3c06dfe86c321d
monusarkar <monusarkar1013@gmail.com>
2025-01-12 20:18:43 +0530

    max rain water

---
+++ Two Pointer/trapping_rain_water.py
@@ -0,0 +1,20 @@
+def Maxwater(arr):
+    n = len(arr)
+    res = 0
+    left, right = 0, n - 1
+    left_max, right_max = arr[0], arr[n - 1]
+
+    while left < right:
+        if left_max <= right_max:
+            left += 1
+            left_max = max(arr[left], left_max)
+            res += left_max - arr[left]
+        else:
+            right -= 1
+            right_max = max(arr[right], right_max)
+            res += right_max - arr[right]
+    return res
+
+
+arr = [3, 0, 1, 0, 4, 0, 2]
+print(Maxwater(arr))
