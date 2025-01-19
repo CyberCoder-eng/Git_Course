ec1f62d9f69ec0e7436380588dc1c91a32994245
monusarkar <monusarkar1013@gmail.com>
2025-01-19 22:47:36 +0530

    rotate linked list

---
+++ Linked List/rotate_linked_list.py
@@ -0,0 +1,17 @@
+# Optimal Code
+
+
+def rotate(self, head, k):
+    temp = head
+    count = 1
+    while temp.next:
+        count += 1
+        temp = temp.next
+    k %= count
+    temp.next = head
+    while k:
+        temp = temp.next
+        k -= 1
+    new_head = temp.next
+    temp.next = None
+    return new_head
