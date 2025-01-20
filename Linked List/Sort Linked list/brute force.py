439ccae6d58f0bb14de77963a70ceb77de9e32dc
monusarkar <monusarkar1013@gmail.com>
2025-01-21 00:11:20 +0530

    merge linked list

---
+++ Linked List/Sort Linked list/brute force.py
@@ -0,0 +1,16 @@
+def sortLinked(head):
+    temp = head
+    res = []
+
+    while temp:
+        res.append(temp.val)
+        temp = temp.next
+
+    res.sort()
+    current_head = head
+    index = 0
+    while current_head:
+        current_head.val = res[index]
+        index += 1
+        current_head = current_head.next
+    return head
