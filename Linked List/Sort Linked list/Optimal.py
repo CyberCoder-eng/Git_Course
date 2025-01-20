e5f650c05fcbb2d0d005350ee391264e94b48159
monusarkar <monusarkar1013@gmail.com>
2025-01-21 00:28:06 +0530

    change code merge linked list

--- Linked List/Sort Linked list/Optimal.py
+++ Linked List/Sort Linked list/Optimal.py
@@ -16,3 +16,26 @@ def mergeLinked(head1, head2):
         Node = Node.next
     Node.next = head1 or head2
     return temp.next
+
+
+# Second solution
+def sortedMerge(self, head1, head2):
+    temp1 = head1
+    temp2 = head2
+    res = Node(-1)
+    ans = res
+
+    while temp1 != None and temp2 != None:
+        if temp1.data < temp2.data:
+            res.next = temp1
+            res = res.next
+            temp1 = temp1.next
+        else:
+            res.next = temp2
+            res = res.next
+            temp2 = temp2.next
+    if temp1 is None:
+        res.next = temp2
+    if temp2 is None:
+        res.next = temp1
+    return ans.next
