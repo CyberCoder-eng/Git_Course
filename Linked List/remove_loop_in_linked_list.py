98dd1b8ff5bd5ad77aefbe37b2e1e8e05d5cc198
monusarkar <monusarkar1013@gmail.com>
2025-01-26 21:08:22 +0530

    remove loop

---
+++ Linked List/remove_loop_in_linked_list.py
@@ -0,0 +1,20 @@
+# Optimal Code
+
+
+def removeLoop(self, head):
+    if not head:
+        return
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            slow = head
+            while slow != fast:
+                slow = slow.next
+                fast = fast.next
+            while fast.next != slow:
+                fast = fast.next
+            fast.next = None
+    return
